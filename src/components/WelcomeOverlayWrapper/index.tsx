b831c57b511dce64d44830468e0f942b8e0dcba1
Arun <chaudharyarun5797@gmail.com>
2025-07-26 15:47:29 +0530

    refactor: comment out unused imports and return null in WelcomeOverlayWrapper

--- src/components/WelcomeOverlayWrapper/index.tsx
+++ src/components/WelcomeOverlayWrapper/index.tsx
@@ -1,9 +1,9 @@
 'use client'
-import React from 'react';
-import WelcomeOverlay from '../WelcomeOverlay';
+// import React from 'react';
+// import WelcomeOverlay from '../WelcomeOverlay';
 
 const WelcomeOverlayWrapper = () => {
-  return <WelcomeOverlay />;
+  return null;
 };
 
 export default WelcomeOverlayWrapper;
